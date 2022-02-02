2c83c48b22e06b99ac2e3191a529717a6798b775
ibrahim1naveed <66309253+ibrahim1naveed@users.noreply.github.com>
2022-02-02 19:22:31 +0500

    hashed the password and added entry to the database (verified)

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 const { check, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
 
 // @route   POST api/users
 // @desc    Register a user
@@ -14,13 +15,40 @@ router.post('/',
     check('email', 'Please include a valid email').isEmail(),
     check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
 ],
-(req,res) => {
+async (req,res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
         
     }
-    res.send('passed');
+    const { name, email, password } = req.body;
+
+    try {
+        let user = await User.findOne({ email: email });
+        if (user) {
+            res.status(400).json({ msg: 'User already exists!' })
+        }
+
+        user = new User({
+            name,
+            email,
+            password
+        });
+
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(password, salt);
+
+        await user.save();
+
+        res.send('User Saved');
+
+
+        
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+        
+    }
 });
 
 module.exports = router;
