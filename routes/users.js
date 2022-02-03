4f994812ac5d80fed06c821cbb71a2db1a53fd9a
ibrahim1naveed <66309253+ibrahim1naveed@users.noreply.github.com>
2022-02-03 23:43:09 +0500

    added payload and sent it through jsonwebtoken

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const User = require('../models/User');
 const { check, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 // @route   POST api/users
 // @desc    Register a user
@@ -40,10 +42,24 @@ async (req,res) => {
 
         await user.save();
 
-        res.send('User Saved');
+        const payload = {
+            user: {
+                id: user.id
+            }
+        };
 
+        jwt.sign(
+            payload,
+            config.get('jwtSecret'),
+            {
+            expiresIn: 360000
+            },
+            (err, token) => {
+                if (err) throw err;
+                res.json({ token });
+            }
+        );
 
-        
     } catch (err) {
         console.error(err.message);
         res.status(500).send('Server Error');
