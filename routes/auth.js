8d2c7fc9e3b1a932dc631620c730ae5e78eaa419
ibrahim1naveed <66309253+ibrahim1naveed@users.noreply.github.com>
2022-02-08 15:50:16 +0500

    added authentication for user using middleware and complete another route

--- routes/auth.js
+++ routes/auth.js
@@ -5,16 +5,24 @@ const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('config');
+const auth = require('../middleware/auth');
 
 // @route   GET api/auth
 // @desc    Get logged in user
 // @access  Private!
-router.get('/', (req,res) => {
-    res.send("Get logged in user")
+router.get('/', auth, async (req,res) => {
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+        res.json(user);   
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error');
+        
+    }
 });
 
 // @router  POST api/auth
-// @desc    Auth user & get token
+// @desc    Auth user & get token (login)
 // @access  Public   
 router.post('/',
  [
