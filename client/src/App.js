a352c277d347ca5146c24279e793738f45eab9de
ibrahim1naveed <66309253+ibrahim1naveed@users.noreply.github.com>
2022-02-21 22:18:26 -0500

    added routes and link for Home and About

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,23 @@
 import './App.css';
 import { Navbar } from './components/layouts/Navbar';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React, { Fragment } from 'react';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+
 
 const App = () => {
   return (
-    <div className="App">
-      <Navbar/>
-      My App
-    </div>
+    <Router>
+        <Navbar/>
+        <div className='container'>
+          <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/about" element={<About/>}/>
+          </Routes>
+        </div>
+    </Router>
   );
-}
+};
 
 export default App;
